4046bb5f3a7600b55a791288b9c3d02cccaa81cd
jejimenezc <je.jimenezcastillo@gmail.com>
2026-04-19 21:02:57 -0400

    fix(webapp): add viewport meta tag to HtmlOutput

--- ui.js
+++ ui.js
@@ -1,6 +1,7 @@
 function doGet() {
   return HtmlService.createTemplateFromFile('Index')
     .evaluate()
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1.0')
     .setTitle('Torneo Tenis de Mesa')
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
